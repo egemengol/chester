d840de938073ae8a7e3c32e0b952c88a783c30a7
Egemen Göl <egemengol@gmail.com>
2024-04-17 02:39:53 +0300

    streamext

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,56 @@
+use v4_manager::StreamOrderBook;
+
 mod core_structs;
 mod v4_manager;
 mod v4_messages;
 
+use futures_util::StreamExt;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    v4_manager::subscribe_to_orderbook(v4_messages::Market::EthUsd).await
+    // v4_manager::subscribe_to_orderbook(v4_messages::Market::EthUsd).await
+    let mut stream = StreamOrderBook::start(v4_messages::Market::EthUsd).await?;
+    let mut stream = stream.stream().await?;
+    while let Some(orderbook_json) = stream.next().await {
+        println!("{}", &orderbook_json);
+    }
+    Ok(())
 }
+
+// use axum::{
+//     extract::ws::{WebSocket, WebSocketUpgrade},
+//     response::{IntoResponse, Response},
+//     routing::get,
+//     Router,
+// };
+
+// async fn handler(ws: WebSocketUpgrade) -> Response {
+//     ws.on_upgrade(handle_socket)
+// }
+
+// async fn handle_socket(mut socket: WebSocket) {
+
+//     while let Some(msg) = socket.recv().await {
+//         let msg = if let Ok(msg) = msg {
+//             msg
+//         } else {
+//             // client disconnected
+//             return;
+//         };
+
+//         if socket.send(msg).await.is_err() {
+//             // client disconnected
+//             return;
+//         }
+//     }
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     // build our application with a single route
+//     let app = Router::new().route("/ws", get(handler));
+
+//     // run our app with hyper, listening globally on port 3000
+//     let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
+//     axum::serve(listener, app).await.unwrap();
+// }
--- src/v4_manager.rs
+++ src/v4_manager.rs
@@ -4,8 +4,9 @@ use tokio::net::TcpStream;
 use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 
 use futures_util::{
-    stream::{SplitSink, SplitStream},
-    SinkExt, StreamExt,
+    future,
+    stream::{self, SplitSink, SplitStream},
+    SinkExt, Stream, StreamExt,
 };
 
 // const NETWORK_ID: &str = "dydx-testnet-4";
@@ -123,4 +124,91 @@ pub async fn subscribe_to_orderbook(market: v4_messages::Market) -> anyhow::Resu
     keep_orderbook(&mut read).await?;
 
     Ok(())
+}
+async fn streaming_keep_orderbook(
+    read: &mut SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+) -> anyhow::Result<impl Stream<Item = String> + '_> {
+    let msg_first = read
+        .next()
+        .await
+        .context("first orderbook message could not be received")?;
+
+    let got_json = match msg_first {
+        Err(e) => unimplemented!("received error during orderbook updates: {}", e),
+        Ok(tokio_tungstenite::tungstenite::Message::Text(t)) => t,
+        Ok(_) => unimplemented!("received nontext message during orderbook updates"),
+    };
+    let subscribed: v4_messages::Subscribed =
+        serde_json::from_str(&got_json).expect("could not deserialize during orderbook incoming");
+
+    let mut orderbook: OrderBookState = subscribed.into();
+
+    let orderbook_json_first =
+        serde_json::to_string(&orderbook).expect("orderbook is able to serialize");
+
+    let stream_first = stream::once(future::ready(orderbook_json_first));
+
+    let out = read.map(move |msg| {
+        let got_json = match msg {
+            Err(e) => unimplemented!("received error during orderbook updates: {}", e),
+            Ok(tokio_tungstenite::tungstenite::Message::Text(t)) => t,
+            Ok(_) => unimplemented!("received nontext message during orderbook updates"),
+        };
+        let got: v4_messages::OrderbookIncomingMessages = serde_json::from_str(&got_json)
+            .expect("could not deserialize during orderbook incoming");
+        match got {
+            v4_messages::OrderbookIncomingMessages::Subscribed(subscribed) => {
+                let mut new_orderbook: OrderBookState = subscribed.into();
+                std::mem::swap(&mut orderbook, &mut new_orderbook)
+            }
+            v4_messages::OrderbookIncomingMessages::ChannelBatchData(channel_batch_data) => {
+                channel_batch_data
+                    .update_orderbook(&mut orderbook)
+                    .context("updating orderbook in stream_overbook")
+                    .unwrap();
+            }
+        }
+        serde_json::to_string(&orderbook).expect("orderbook is able to serialize")
+    });
+
+    Ok(stream_first.chain(out))
+}
+
+// pub async fn stream_orderbook_json(
+//     market: v4_messages::Market,
+// ) -> anyhow::Result<impl Stream<Item = std::string::String>> {
+//     let (stream, _) = connect_async(TESTNET_INDEXER_WS_HOST)
+//         .await
+//         .context("Failed to connect")?;
+
+//     let (mut write, mut read) = stream.split();
+
+//     let _ = recv_connected_msg(&mut read).await?;
+//     send_subscribe_msg(&mut write, market).await?;
+//     let orderbooks = streaming_keep_orderbook(&mut read).await?;
+
+//     Ok(orderbooks)
+// }
+
+pub struct StreamOrderBook {
+    read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+}
+
+impl StreamOrderBook {
+    pub async fn start(market: v4_messages::Market) -> anyhow::Result<Self> {
+        let (stream, _) = connect_async(TESTNET_INDEXER_WS_HOST)
+            .await
+            .context("Failed to connect")?;
+
+        let (mut write, mut read) = stream.split();
+
+        let _ = recv_connected_msg(&mut read).await?;
+        send_subscribe_msg(&mut write, market).await?;
+        Ok(Self { read })
+    }
+
+    pub async fn stream(&mut self) -> anyhow::Result<impl Stream<Item = std::string::String> + '_> {
+        let orderbooks = streaming_keep_orderbook(&mut self.read).await?;
+        Ok(orderbooks)
+    }
 }
